df4d87ab085503620f1a6760a0e5c0dad09290ab
Xinck7 <Nickolaus.vendel@gmail.com>
2023-01-28 22:16:48 -0500

    Nav bar completed! deleted unused code

--- backend/grocery_helper_project/grocery_helper_api/models.py
+++ backend/grocery_helper_project/grocery_helper_api/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-from rest_framework.authtoken.models import Token
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100, help_text="name of ingredient")
