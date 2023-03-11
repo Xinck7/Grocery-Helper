6af0099440860a66de841a058539d71b007fd69a
Xinck7 <Nickolaus.vendel@gmail.com>
2023-03-11 01:29:16 -0500

    item shows up from api and started add to list button

---
+++ frontend/GroceryHelper/src/app/model/items.model.ts
@@ -0,0 +1,9 @@
+export class Item {
+    public name: string | undefined;
+    public location: string | undefined;
+    public ailse: number | undefined;
+    public price: number | undefined;
+    public picture: HTMLImageElement | undefined;
+    public added_by: string | undefined;
+    public show_only_your_added_by : boolean | undefined;
+}
